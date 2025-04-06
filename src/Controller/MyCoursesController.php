31125d7e203ac66cb3612123cb818697372bc977
AntoineMilo <antoinemacmil45@gmail.com>
2025-04-06 15:33:50 +0200

    mise en commentaire connexion a la bdd pour premier rendu

--- src/Controller/MyCoursesController.php
+++ src/Controller/MyCoursesController.php
@@ -14,14 +14,32 @@ class MyCoursesController extends AbstractController
     public function index(EntityManagerInterface $entityManager): \Symfony\Component\HttpFoundation\Response
     {
         // Récupérer l'utilisateur avec l'ID 1
-        $user = $entityManager->getRepository(User::class)->find(1);
+        // $user = $entityManager->getRepository(User::class)->find(1);
 
-        if (!$user) {
-            throw $this->createNotFoundException('Utilisateur non trouvé');
-        }
+        // if (!$user) {
+        //     throw $this->createNotFoundException('Utilisateur non trouvé');
+        // }
 
         // Récupérer les cours auxquels l'utilisateur est inscrit
-        $courses = $user->getUe();
+        // $courses = $user->getUe();
+
+        // Utilisation de cours statiques pour le moment
+        $courses = [
+            (object) ['id' => 1, 'nom' => 'Cours de Mathématiques', 'code' => 'MATH101', 'image' => 'course_image.png', 'description' => 'Introduction aux mathématiques.'],
+            (object) ['id' => 2, 'nom' => 'Cours de Physique', 'code' => 'PHYS101', 'image' => 'course_image.png', 'description' => 'Introduction à la physique.'],
+            (object) ['id' => 3, 'nom' => 'Cours de Mathématiques', 'code' => 'MATH101', 'image' => 'course_image.png', 'description' => 'Introduction aux mathématiques.'],
+            (object) ['id' => 4, 'nom' => 'Cours de Physique', 'code' => 'PHYS101', 'image' => 'course_image.png', 'description' => 'Introduction à la physique.'],
+            (object) ['id' => 5, 'nom' => 'Cours de Mathématiques', 'code' => 'MATH101', 'image' => 'course_image.png', 'description' => 'Introduction aux mathématiques.'],
+            (object) ['id' => 6, 'nom' => 'Cours de Physique', 'code' => 'PHYS101', 'image' => 'course_image.png', 'description' => 'Introduction à la physique.'],
+            (object) ['id' => 1, 'nom' => 'Cours de Mathématiques', 'code' => 'MATH101', 'image' => 'course_image.png', 'description' => 'Introduction aux mathématiques.'],
+            (object) ['id' => 2, 'nom' => 'Cours de Physique', 'code' => 'PHYS101', 'image' => 'course_image.png', 'description' => 'Introduction à la physique.'],
+            (object) ['id' => 3, 'nom' => 'Cours de Mathématiques', 'code' => 'MATH101', 'image' => 'course_image.png', 'description' => 'Introduction aux mathématiques.'],
+            (object) ['id' => 4, 'nom' => 'Cours de Physique', 'code' => 'PHYS101', 'image' => 'course_image.png', 'description' => 'Introduction à la physique.'],
+            (object) ['id' => 5, 'nom' => 'Cours de Mathématiques', 'code' => 'MATH101', 'image' => 'course_image.png', 'description' => 'Introduction aux mathématiques.'],
+            (object) ['id' => 6, 'nom' => 'Cours de Physique', 'code' => 'PHYS101', 'image' => 'course_image.png', 'description' => 'Introduction à la physique.'],
+            (object) ['id' => 5, 'nom' => 'Cours de Mathématiques', 'code' => 'MATH101', 'image' => 'course_image.png', 'description' => 'Introduction aux mathématiques.'],
+            (object) ['id' => 6, 'nom' => 'Cours de Physique', 'code' => 'PHYS101', 'image' => 'course_image.png', 'description' => 'Introduction à la physique.'],
+        ];
 
         return $this->render('myCourses/myCourses.html.twig', [
             'courses' => $courses,
@@ -32,26 +50,33 @@ class MyCoursesController extends AbstractController
     public function getCourses(EntityManagerInterface $entityManager): JsonResponse
     {
         // Récupérer l'utilisateur avec l'ID 1
-        $user = $entityManager->getRepository(User::class)->find(1);
+        // $user = $entityManager->getRepository(User::class)->find(1);
 
-        if (!$user) {
-            return new JsonResponse(['error' => 'Utilisateur non trouvé'], 404);
-        }
+        // if (!$user) {
+        //     return new JsonResponse(['error' => 'Utilisateur non trouvé'], 404);
+        // }
 
         // Récupérer les cours auxquels l'utilisateur est inscrit
-        $courses = $user->getUe();
+        // $courses = $user->getUe();
 
-        $courseData = [];
-        foreach ($courses as $course) {
-            $courseData[] = [
-                'id' => $course->getId(),
-                'nom' => $course->getNom(),
-                'code' => $course->getCode(),
-                'image' => $course->getImage(),
-                'description' => $course->getDescription(),
-            ];
-        }
+        // Utilisation de cours statiques pour le moment
+        $courses = [
+            (object) ['id' => 1, 'nom' => 'Cours de Mathématiques', 'code' => 'MATH101', 'image' => 'course_image.png', 'description' => 'Introduction aux mathématiques.'],
+            (object) ['id' => 2, 'nom' => 'Cours de Physique', 'code' => 'PHYS101', 'image' => 'course_image.png', 'description' => 'Introduction à la physique.'],
+            (object) ['id' => 3, 'nom' => 'Cours de Mathématiques', 'code' => 'MATH101', 'image' => 'course_image.png', 'description' => 'Introduction aux mathématiques.'],
+            (object) ['id' => 4, 'nom' => 'Cours de Physique', 'code' => 'PHYS101', 'image' => 'course_image.png', 'description' => 'Introduction à la physique.'],
+            (object) ['id' => 5, 'nom' => 'Cours de Mathématiques', 'code' => 'MATH101', 'image' => 'course_image.png', 'description' => 'Introduction aux mathématiques.'],
+            (object) ['id' => 6, 'nom' => 'Cours de Physique', 'code' => 'PHYS101', 'image' => 'course_image.png', 'description' => 'Introduction à la physique.'],
+            (object) ['id' => 1, 'nom' => 'Cours de Mathématiques', 'code' => 'MATH101', 'image' => 'course_image.png', 'description' => 'Introduction aux mathématiques.'],
+            (object) ['id' => 2, 'nom' => 'Cours de Physique', 'code' => 'PHYS101', 'image' => 'course_image.png', 'description' => 'Introduction à la physique.'],
+            (object) ['id' => 3, 'nom' => 'Cours de Mathématiques', 'code' => 'MATH101', 'image' => 'course_image.png', 'description' => 'Introduction aux mathématiques.'],
+            (object) ['id' => 4, 'nom' => 'Cours de Physique', 'code' => 'PHYS101', 'image' => 'course_image.png', 'description' => 'Introduction à la physique.'],
+            (object) ['id' => 5, 'nom' => 'Cours de Mathématiques', 'code' => 'MATH101', 'image' => 'course_image.png', 'description' => 'Introduction aux mathématiques.'],
+            (object) ['id' => 6, 'nom' => 'Cours de Physique', 'code' => 'PHYS101', 'image' => 'course_image.png', 'description' => 'Introduction à la physique.'],
+            (object) ['id' => 5, 'nom' => 'Cours de Mathématiques', 'code' => 'MATH101', 'image' => 'course_image.png', 'description' => 'Introduction aux mathématiques.'],
+            (object) ['id' => 6, 'nom' => 'Cours de Physique', 'code' => 'PHYS101', 'image' => 'course_image.png', 'description' => 'Introduction à la physique.'],
+        ];
 
-        return new JsonResponse($courseData);
+        return new JsonResponse($courses);
     }
 }
