616d311ed92baede28b9598c1bddb1f4fd700599
Kingwizar <nizar07022004@gmail.com>
2025-04-27 23:11:51 +0200

    notifications POST

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -2,14 +2,16 @@
 
 namespace App\Controller;
 
-use App\Repository\NotificationRepository;
+use App\Entity\Notification;
+use App\Entity\UserNotif;
 use App\Repository\UserNotifRepository;
+use App\Repository\UserUeRepository;
+use App\Repository\TypeNotifRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class NotificationController extends AbstractController
 {
@@ -33,4 +35,69 @@ class NotificationController extends AbstractController
         return new JsonResponse(['status' => 'success']);
     }
 
+    #[Route('/notification/create', name: 'notification_create', methods: ['POST'])]
+    public function createNotification(
+        Request $request,
+        EntityManagerInterface $em,
+        UserUeRepository $userUeRepository,
+        TypeNotifRepository $typeNotifRepository
+    ): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (!isset($data['ueId'], $data['message'], $data['type'])) {
+            return new JsonResponse(['success' => false, 'error' => 'Paramètres manquants'], 400);
+        }
+
+        $ueId = $data['ueId'];
+        $message = $data['message'];
+        $type = strtolower($data['type']);
+
+        // Traduction du type reçu en type de notification existant
+        $typeMap = [
+            'low' => 'info',
+            'medium' => 'important',
+            'high' => 'warning',
+            'info' => 'info',
+            'important' => 'important',
+            'warning' => 'warning',
+        ];
+
+        if (!array_key_exists($type, $typeMap)) {
+            return new JsonResponse(['success' => false, 'error' => 'Type de notification invalide'], 400);
+        }
+
+        $finalType = $typeMap[$type];
+
+        // Chercher le type_notif correspondant
+        $typeNotif = $typeNotifRepository->findOneBy(['typeNotif' => $finalType]);
+        if (!$typeNotif) {
+            return new JsonResponse(['success' => false, 'error' => 'Type de notification introuvable'], 404);
+        }
+
+        // Créer la notification
+        $notification = new Notification();
+        $notification->setMessage($message);
+        $notification->setDate(new \DateTimeImmutable());
+        $notification->setTypeNotif($typeNotif);
+
+        $em->persist($notification);
+        $em->flush();
+
+        // Récupérer les utilisateurs de cette UE
+        $userUes = $userUeRepository->findBy(['ue' => $ueId]);
+
+        foreach ($userUes as $userUe) {
+            $userNotif = new UserNotif();
+            $userNotif->setUsersId($userUe->getUser()->getId());
+            $userNotif->setNotification($notification);
+            $userNotif->setEstVu(false);
+
+            $em->persist($userNotif);
+        }
+
+        $em->flush();
+
+        return new JsonResponse(['success' => true]);
+    }
 }
